9f275ded4105ea9a03df798c5482c6278e5f2be0
大森裕太朗 <kurimeshi@oomorihiroshitarounoMacBook-Pro.local>
2020-08-23 18:27:27 +0900

    adding new file

---
+++ gameplayer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 23 12:45:34 2020
+
+@author: kurimeshi
+"""
+
+import sys
+sys.path.append('Users/kurimeshi/numeron/')
+import numeronupgrade as nu
+
+nu.Game.play_game()
+
